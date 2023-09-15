34f9876ed43741fb0f3877d40bb2ccada26c1b8a
RanggaNf <mohammad.rangga.n.f@gmail.com>
2023-09-15 21:56:49 +0700

    Event Wrapper Class

--- app/src/main/java/com/dicoding/restaurantreview/ui/MainActivity.kt
+++ app/src/main/java/com/dicoding/restaurantreview/ui/MainActivity.kt
@@ -43,9 +43,15 @@ class MainActivity : AppCompatActivity() {
             showLoading(it)
         }
 
-        mainViewModel.snackbarText.observe(this) {
-            Snackbar.make(window.decorView.rootView, it, Snackbar.LENGTH_SHORT).show()
-        }
+        mainViewModel.snackbarText.observe(this, {
+            it.getContentIfNotHandled()?.let { snackBarText ->
+                Snackbar.make(
+                    window.decorView.rootView,
+                    snackBarText,
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }
+        })
 
         binding.btnSend.setOnClickListener { view ->
             mainViewModel.postReview(binding.edReview.text.toString())
--- app/src/main/java/com/dicoding/restaurantreview/ui/MainViewModel.kt
+++ app/src/main/java/com/dicoding/restaurantreview/ui/MainViewModel.kt
@@ -12,6 +12,7 @@ import com.dicoding.restaurantreview.data.retrofit.ApiConfig
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import com.dicoding.restaurantreview.util.Event
 
 class MainViewModel : ViewModel() {
 
@@ -24,8 +25,8 @@ class MainViewModel : ViewModel() {
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> = _isLoading
 
-    private val _snackbarText = MutableLiveData<String>()
-    val snackbarText: LiveData<String> = _snackbarText
+    private val _snackbarText = MutableLiveData<Event<String>>()
+    val snackbarText: LiveData<Event<String>> = _snackbarText
     companion object{
         private const val TAG = "MainViewModel"
         private const val RESTAURANT_ID = "uewq1zg2zlskfw1e867"
@@ -64,7 +65,7 @@ class MainViewModel : ViewModel() {
                 _isLoading.value = false
                 if (response.isSuccessful) {
                     _listReview.value = response.body()?.customerReviews
-                    _snackbarText.value = response.body()?.message
+                    _snackbarText.value = Event(response.body()?.message.toString())
                 } else {
                     Log.e(TAG, "onFailure: ${response.message()}")
                 }
